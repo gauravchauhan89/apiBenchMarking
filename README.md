28321044e1e9b8dc5e8db826181314b0e937a407
gauravroxs <gaurav.the.iiitian@gmail.com>
2015-03-18 12:27:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Dependency:
+Apache Benchmark - http://httpd.apache.org/docs/2.2/programs/ab.html, https://code.google.com/p/apachebench-standalone/wiki/HowToBuild
+gnuplot
---
+++ src/benchmark/BenchMark.java
@@ -0,0 +1,99 @@
+package benchmark;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.text.StrBuilder;
+
+public class BenchMark {
+	//private static final String abCmd = "ab -n 100 -c 10 -g {DATA_FILENAME}.txt -p post_data.txt -T application/json -H \"x-forwarded-for: 171.48.0.1\" -H \"x-bsy-did: {DEVICE_ID}\" -H \"x-bsy-utkn: {UID}:{TOKEN}\" -H \"x-client: map\" {API_URL}";
+	private static final String gnuplotPath = "/opt/local/bin/gnuplot";
+	private static final String abMainCmd = "-n 50 -c 10 -T application/json";
+	private static Map<String, String> apiUrlMap = new HashMap<String, String>();
+	
+	static {
+		
+		apiUrlMap.put("products", "http://10.0.3.113/myairtelapp/v1/account/products?spoof=true");
+		apiUrlMap.put("genericInfo", "http://10.0.3.113/myairtelapp/v1/account/genericInfo?spoof=true");
+		apiUrlMap.put("customTiles", "http://10.0.3.113/myairtelapp/v1/account/customTiles?spoof=true");
+		apiUrlMap.put("alerts", "http://10.0.3.113/myairtelapp/v1/account/alerts?spoof=true");
+	}
+	
+	public static void main(String[] args) {
+		URL url = BenchMark.class.getResource("user_info.txt");
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(url.getPath())));
+			br.readLine();
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				String[] infoArray = line.split(" ");
+				String msisdn = infoArray[0];
+				String deviceId = infoArray[1];
+				String uId = infoArray[2];
+				String token = infoArray[3];
+				System.out.println(msisdn+" "+deviceId+" "+token +" "+ uId);
+				
+				for(Map.Entry<String, String> apiInfo : apiUrlMap.entrySet()) {
+					String dataFileName = msisdn+"-"+apiInfo.getKey();
+					dataFileName = dataFileName.trim();
+					String apiUrl = apiInfo.getValue();
+					String[] cmd = new String[8];
+					cmd[0] = "ab";
+					cmd[1] = abMainCmd;
+					cmd[2] = "-g"+dataFileName+".txt";
+					cmd[3] = "-H \"x-forwarded-for: 171.48.0.1\"";
+					cmd[4] = "-H \"x-client: map\"";
+					cmd[5] = "-H \"x-bsy-did: "+deviceId+"\"";
+					cmd[6] = "-H \"x-bsy-utkn: "+uId+":"+token+"\"";
+					cmd[7] = apiUrl;
+					
+					Process p = Runtime.getRuntime().exec(cmd);
+					System.out.println(apiUrl+"\nProcessing ...");
+					
+					BufferedReader reader = 
+					         new BufferedReader(new InputStreamReader(p.getErrorStream()));
+					String line1 = "";
+				    while ((line1 = reader.readLine())!= null) {
+				    		System.out.println(line1);
+				    }
+				    p.waitFor();
+				    
+				    // Ploting graph
+				    URL plot_url = BenchMark.class.getResource("benchmark.plot");
+				    String script_text = new Scanner(new File(plot_url.getPath())).useDelimiter("\\Z").next();
+				    StrBuilder script = new StrBuilder(script_text);
+				    script.replaceAll("{DATAFILE}", dataFileName);
+				    
+				    FileWriter fw = new FileWriter( (new File("benchmark1.plot")).getAbsoluteFile( ) );
+				    BufferedWriter bw = new BufferedWriter( fw );
+				    bw.write( script.toString() );
+				    bw.close();
+				    
+				    Process gnuplot = Runtime.getRuntime().exec(gnuplotPath+" benchmark1.plot");
+				    gnuplot.waitFor();
+				    
+				    BufferedReader reader1 = 
+					         new BufferedReader(new InputStreamReader(gnuplot.getErrorStream()));
+					String line2 = "";
+				    while ((line2 = reader1.readLine())!= null) {
+				    		System.out.println(line2);
+				    }
+				    
+				    System.out.println("Done.");
+				}
+			}
+		 
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
